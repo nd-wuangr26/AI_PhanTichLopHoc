d699d1f2b0e9eec172d57802b0aa44ca0fbe230e
AI_PhanTichLopHoc <nd.wuang26@gmail.com>
2025-05-09 02:47:32 +0700

    chuyen sang onnx de inference

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-from my_model import load_model_and_predict
-import cv2
-from collections import Counter
-
-frame = cv2.imread("test_frame.jpg")
-labels = load_model_and_predict(frame)
-print(labels)
-counts = Counter(labels)
-study_count = counts.get("study", 0)
-sleep_count = counts.get("sleep", 0)
-other_count = counts.get("other", 0)
-print(study_count, sleep_count, other_count)
+from ultralytics import YOLO
+model = YOLO("model/best.pt")
+print(model.names)
